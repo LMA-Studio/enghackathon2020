118315f2abad8e5256475b5c61fa653bfb2e9834
Andreas Brake <andreas@andreasbrake.ca>
2020-06-24 18:27:29 -0700

    added licensing and header formats

--- StreamVR.TestClient/Program.cs
+++ StreamVR.TestClient/Program.cs
@@ -1,14 +1,32 @@
-﻿using Newtonsoft.Json;
+﻿/*
+    This file is part of LMAStudio.StreamVR
+    Copyright(C) 2020  Andreas Brake, Lisa-Marie Mueller
+
+    LMAStudio.StreamVR is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Utility;
-using Utility.Models;
+using LMAStudio.StreamVR.Common;
+using LMAStudio.StreamVR.Common.Models;
 
-namespace TestConnector.TestClient
+namespace LMAStudio.StreamVR.TestClient
 {
     class Program
     {
