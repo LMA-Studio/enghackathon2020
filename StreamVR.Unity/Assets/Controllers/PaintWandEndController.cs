46694d1a98a9fc541c20f7f9d9efa423fa086533
Andreas Brake <andreas@andreasbrake.ca>
2020-06-26 00:36:45 -0700

    Added UI for paint wand

---
+++ StreamVR.Unity/Assets/Controllers/PaintWandEndController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintWandEndController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "paint")
+        {
+            Debug.Log("Attaching Paint");
+
+            Material paintMaterial = collision.gameObject.GetComponent<MeshRenderer>().material;
+
+            GameObject parent = this.transform.parent.gameObject;
+            ShootingPaintScript paintShooter = parent.GetComponent<ShootingPaintScript>();
+
+            paintShooter.ChangePaint(paintMaterial);
+            this.GetComponent<MeshRenderer>().material = paintMaterial;
+        }
+    }
+}
--- StreamVR.Unity/Assets/Scripts/ShootingPaintScript.cs
+++ StreamVR.Unity/Assets/Scripts/ShootingPaintScript.cs
@@ -1,19 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class ShootingPaintScript : MonoBehaviour
 {
+    public XRNode inputSource;
+
     public float speed = 40;
+
     public GameObject paintBlob;
     public Transform barrel;
     public AudioSource audioSource;
     public AudioClip audioClip;
 
+    private bool clicking = false;
+
+    private void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+
+        bool clicked;
+        device.TryGetFeatureValue(CommonUsages.triggerButton, out clicked);
+
+        if (clicked)
+        {
+            if (!clicking)
+            {
+                clicking = true;
+                Fire();
+            }
+        }
+        else
+        {
+            clicking = false;
+        }
+
+        bool gripped;
+        device.TryGetFeatureValue(CommonUsages.gripButton, out gripped);
+
+        if (gripped)
+        {
+            var pointer = this.transform.parent.gameObject.GetComponentInChildren<PhysicsPointer>(true)?.gameObject;
+            if (pointer != null)
+            {
+                this.gameObject.SetActive(false);
+                pointer.SetActive(true);
+            }
+        }
+    }
+
+    public void ChangePaint(Material newPaint)
+    {
+        paintBlob.GetComponent<MeshRenderer>().material = newPaint;
+    }
+
     public void Fire()
     {
         GameObject spawnedPaint = Instantiate(paintBlob, barrel.position, barrel.rotation);
-        spawnedPaint.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        spawnedPaint.GetComponent<Rigidbody>().velocity = speed * barrel.up;
         audioSource.PlayOneShot(audioClip);
         Destroy(spawnedPaint, 2);
     }
